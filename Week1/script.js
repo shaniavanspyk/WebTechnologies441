a453bc35a620d8ab9f666ffef5f72dd690ef8f8b
shaniavanspyk <shaniavanspyk@umconnect.umt.edu>
2026-01-25 17:52:37 -0700

    Update script.js

--- Week1/script.js
+++ Week1/script.js
@@ -1,8 +1,8 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e){
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior : "smooth"
-        });
-    });
-});
+const menuIcon = document.querySelector
+('#menu-icon');
+const navbar = document.querySelector('navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}

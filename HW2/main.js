eb4a583ec4d6c7c58e739c82daad5c50c7fef072
shaniavanspyk <shaniavanspyk@umconnect.umt.edu>
2026-01-28 13:48:27 -0700

    d

---
+++ HW2/main.js
@@ -0,0 +1,22 @@
+//My favorite Websites
+console.log("My favorite websites are");
+console.log("1. Google.com");
+console.log("2. YouTube.com");
+console.log("3. Pintrest.com");
+
+// My favorite Games
+console.log("My favorite games are");
+console.log("1.Clash Royale");
+console.log("2.Any Card Game");
+console.log("3.Monopoly");
+
+// My favorite Artists & Cultural Importance
+console.log("My favorite artists and their cultural importance are");
+console.log("1.Hozier,Irish song-writer, Social and Politica Activism, Mythology and Literature, and Irish Identity are just a few.");
+console.log("2.Vincent van Gogh, Western art, Artistic Innovation, Global Popularity, and Influence.");
+console.log("3.Millie Bobby Brown, actress, Advocacy, Cultral Trends, Empowerment, and much more.");
+
+let yourAnswer = prompt("What is your favorite Game?");
+
+console.log("User Interaction:");
+console.log("Yous favorite food is: " + yourAnswer);
